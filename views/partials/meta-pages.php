080de0181e2172a56024f19c2cdb66ef3dfcd381
mike-stumpf <mike.a.stumpf@gmail.com>
2016-01-22 23:00:08 -0500

    reestablish master

--- views/partials/meta-pages.php
+++
@@ -1,3 +0,0 @@
-<p>
-    <input class="wprl-pages-input" type="text" name="wprl-pages" id="wprl-pages-admin" value="<?php echo esc_attr(get_post_meta($object->ID, 'wprl_pages', true)); ?>" size="30" onchange="pageCheck()"/>
-</p>
